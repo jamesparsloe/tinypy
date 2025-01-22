38b0b8d0b85677ab048ce4ec9790b5280eaf6f84
James Parsloe <jamesparsloe@gmail.com>
2025-01-22 11:12:34 +0000

    Fix exit codes

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 output=$(uv run tinypy examples/fibonacci.py)
 if [ "$output" = "55" ]; then
     echo "Test passes!"
+    exit 0
 else
     echo "Test failed: expected 55 but got '$output'"
+    exit 1
 fi
